e5ac18057e29a08f733e6d2dc45f374f3ebf29da
Martin Leopold <m@martinleopold.com>
2018-01-16 13:10:58 +0100

    Save simplified data to file

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const getsheet = require('./index');
 
 // getsheet.getRaw('19R40EvDrld96u1qg--hJpGDTOwvgzz532ZAc2T6FWU4', '.client_secret.json').then(data => {
@@ -7,4 +8,19 @@ const getsheet = require('./index');
 getsheet.getSimplified('19R40EvDrld96u1qg--hJpGDTOwvgzz532ZAc2T6FWU4', '.client_secret.json').then(data => {
   console.log(data);
   console.log(data.sheets[0].rows);
+  store(data);
 });
+
+
+function store(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(data.spreadsheetId + "_" + new Date().toISOString() + ".json", JSON.stringify(data, null, 2), err => {
+      if (err) {
+        console.log('error', err);
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
